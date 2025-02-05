f3229a8a311a32318e05294fbd1146eea10269c4
Saaransh Jain <saaransh.x.jain@gmail.com>
2025-02-05 05:19:15 +0000

    Initial commit

---
+++ lexer.c
@@ -0,0 +1,11 @@
+FILE *getStream(FILE *fp) {
+    // to be implemented
+}
+
+tokenInfo getNextToken(twinBuffer B) {
+    // to be implemented
+}
+
+void removeComments(char *testcaseFile, char *cleanFile) {
+    // to be implemented
+}
---
+++ parser.c
@@ -0,0 +1,15 @@
+FirstAndFollow ComputeFirstAndFollowSets (grammar G) {
+    // to be implemented
+}
+
+void createParseTable(FirstAndFollow F, table T) {
+    // to be implemented
+}
+
+void parseInputSourceCode(char *testcaseFile, table T) {
+    // to be implemented
+}
+
+void printParseTree(parseTree PT, char *outfile) {
+    // to be implemented
+}
