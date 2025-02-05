4966ed0d85e8db6c05551a635022ae35f77afe65
Vishnu Hari <33627388+lycogno@users.noreply.github.com>
2025-02-05 16:23:26 +0530

    Add gitignore and driver skeleton

--- driver.c
+++ driver.c
@@ -1,3 +1,41 @@
-int main() {
-    return 0;
+#include <stdio.h>
+
+// Lexer and parser headers
+#include "lexer.h"
+#include "parser.h"
+
+int main(int argc, char *argv[]) {
+    if(argc != 3){
+        printf("Usage: %s <testcase file> <clean file>\n", argv[0]);
+        return 1;
+    }
+    printf("Implementation status: Unimplemented\n");
+    printf("------------------------------------\n");
+    while(1){
+        printf("Choose an option:\n");
+        printf("0. Exit\n");
+        printf("1. Remove Comments\n");
+        printf("2. Print Tokens\n");
+        printf("3. Generate Parse Tree\n");
+        int input;
+        scanf("%d", &input);
+        switch(input){
+            case 0:
+                return 0;
+            case 1:
+                printf("Deleting Comments:\n");
+                // removeComments(argv[1], argv[2]);
+                break;
+            case 2:
+                printf("Printing Tokens:\n");
+                break;
+            case 3:
+                printf("Generating parse tree\n");
+                break;
+            default:
+                printf("Invalid input\n");
+                break;
+        }
+        printf("------------------------------------\n");
+    }
 }
---
+++ lexer.h
@@ -0,0 +1,1 @@
+void removeComments(char *testcaseFile, char *cleanFile);
