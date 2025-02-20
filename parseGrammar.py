c79bf29876d0445da7742a252b8946d00f12d615
Vishnu Hari <33627388+lycogno@users.noreply.github.com>
2025-02-21 04:45:58 +0530

    Parse grammar file to C header
    Added a python script to parse simply formatted grammar file into a C header file
    (grammar.h). Made corresponding changes in grammar file. Note that epsilon is
    treated as if it were TK_ID, but can be detected by is_epsilon.

---
+++ parseGrammar.py
@@ -0,0 +1,67 @@
+# NOTE: This file is not part of the final compiler. It is used to parse the grammar rules written in a simple format and convert them into a C header file.
+HEADER_FILE = "grammar.h"
+SOURCE_GRAMMAR_FILE = "grammar.txt"
+
+def parseGrammar():
+    with open(SOURCE_GRAMMAR_FILE, "r") as f:
+        lines = f.readlines()
+    
+    with open(HEADER_FILE, "w") as f:
+        f.write("""#ifndef GRAMMAR_H
+#define GRAMMAR_H
+#include "parserDef.h"\n""")
+        seen_symbols = set()
+        rule_num = 0
+        for line in lines:
+            rule = line.split("-->")
+            if len(rule) != 2:
+                print("Invalid rule: ", line)
+                continue
+            lhs = rule[0].strip()
+            rhs_arr = rule[1].strip().split("|")
+            for rhs in rhs_arr:
+                rhs = rhs.strip().split(" ")
+                rule_rhs = []
+                for rhs_elem in rhs:
+                    is_epsilon = rhs_elem == "eps"
+                    is_terminal = not (rhs_elem[0] == "<" and rhs_elem[-1] == ">")
+
+                    if is_terminal:
+                        rule_rhs.append(rhs_elem.lower())
+                        if rhs_elem in seen_symbols:
+                            continue
+                        f.write("struct ruleRightSideElement " + rhs_elem.lower() + " = {")
+                        f.write(str(is_epsilon).lower() + ", " + str(is_terminal).lower() + ", ")
+                        f.write(f".symbol.terminal = {'TK_ID' if is_epsilon else rhs_elem}")
+                        f.write("};\n")
+
+                    else:
+                        rule_rhs.append(rhs_elem[1:-1])
+                        if rhs_elem in seen_symbols:
+                            continue
+                        f.write("struct ruleRightSideElement " + rhs_elem[1:-1] + " = {")
+                        f.write(str(is_epsilon).lower() + ", " + str(is_terminal).lower() + ", ")
+                        f.write(".symbol.nonTerminal = NT_" + rhs_elem[1].capitalize() + rhs_elem[2:-1])
+                        f.write("};\n")
+                    seen_symbols.add(rhs_elem)
+
+                f.write ("RuleRightSideElement rhs" + str(rule_num) + "[] = {")
+                for rhs_elem in rule_rhs:
+                    f.write(f"&{rhs_elem}, ")
+                f.write("};\n")
+                f.write("struct rule rule" + str(rule_num) + " = {")
+                f.write(".lhs = NT_" + lhs[1].capitalize() + lhs.strip()[2:-1] + ", ")
+                f.write(".rhs = rhs" + str(rule_num) + ", ")
+                f.write(".rhsLength = " + str(len(rule_rhs)) + "};\n")
+                rule_num += 1
+        f.write("\nRule rules[] = {")
+        for i in range(rule_num):
+            f.write("&rule" + str(i) + ", ")
+        f.write("};\n")
+        f.write("struct grammar languageGrammar = {NT_Program, " + str(rule_num) + ", rules};\n")
+        f.write("#define NUM_NON_TERMINALS " + str(len(seen_symbols)) + "\n")
+        f.write("#define NUM_RULES " + str(rule_num) + "\n")
+        f.write("#endif\n")
+
+if __name__ == "__main__":
+    parseGrammar()
