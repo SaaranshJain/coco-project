27c795b6d6704151818e0cf9e41d4b774dcf632b
Aman Patel <aman030904@gmail.com>
2025-03-13 17:30:52 +0530

    Added the memoization logic & testing code for first&follow Set

--- src/parser.c
+++ src/parser.c
@@ -1,8 +1,7 @@
-#include "parser.h"
-#include "grammar.h"
-#include "lexer.h"
-#include "lookup.h"
-#include "firstAndFollow.h"
+#include "../includes/parser.h"
+#include "../includes/grammar.h"
+#include "../includes/lexer.h"
+#include "../includes/firstAndFollow.h"
 #include <stdlib.h>
 
 FirstAndFollow *ComputeFirstAndFollowSets(Grammar G) {
@@ -27,12 +26,14 @@ FirstAndFollow *ComputeFirstAndFollowSets(Grammar G) {
 
         firstAndFollowSets[i]->nonTerminal = (enum NON_TERMINAL)(i + NUM_TERMINALS);
         firstAndFollowSets[i]->firstSetSize = 0;
+        firstAndFollowSets[i]->firstSetCapacity = 1;
         firstAndFollowSets[i]->firstSetEpsilon = false;
         firstAndFollowSets[i]->followSetSize = 0;
+        firstAndFollowSets[i]->followSetCapacity = 1;
         firstAndFollowSets[i]->followSetDollar = false;
 
         // Initial allocation for sets (dynamic resizing later)
-        int initialCapacity = 5;
+        int initialCapacity = 1;
         firstAndFollowSets[i]->firstSet = (enum TOKEN_TYPE *)malloc(initialCapacity * sizeof(enum TOKEN_TYPE));
         if (!firstAndFollowSets[i]->firstSet) {
             perror("Memory allocation failed");
@@ -61,57 +62,95 @@ FirstAndFollow *ComputeFirstAndFollowSets(Grammar G) {
         }
     }
 
+    // A dp boolean array to check the completeness of the first or follow set of a non-terminal
+    bool* memoized = (bool*)malloc(NUM_NON_TERMINALS * sizeof(bool));
+    if (!memoized) {
+        perror("Memory allocation failed");
+        // Clean up previously allocated memory
+        for (int j = 0; j < NUM_NON_TERMINALS; j++) {
+            free(firstAndFollowSets[j]->firstSet);
+            free(firstAndFollowSets[j]->followSet);
+            free(firstAndFollowSets[j]);
+        }
+        free(firstAndFollowSets);
+        return NULL;
+    }
+
+    // Initializing all to False
+    for (int i = 0; i < NUM_NON_TERMINALS; i++) { 
+        memoized[i] = false;
+    }
+
     // Compute FIRST sets for all non-terminals
     for (int i = 0; i < NUM_NON_TERMINALS; i++) {
-        computeFirstSet(G, firstAndFollowSets, (enum NON_TERMINAL)(i + NUM_TERMINALS));
+        computeFirstSet(G, firstAndFollowSets, (enum NON_TERMINAL)(i + NUM_TERMINALS), memoized);
+    }
+
+    // Again initializing all to False for follow set computation
+    for (int i = 0; i < NUM_NON_TERMINALS; i++) { 
+        memoized[i] = false; 
     }
 
     // Compute FOLLOW sets for all non-terminals
-    computeFollowSet(G, firstAndFollowSets, G->startSymbol);
+    computeFollowSet(G, firstAndFollowSets, G->startSymbol, memoized);
     for (int i = 0; i < NUM_NON_TERMINALS; i++) {
-        computeFollowSet(G, firstAndFollowSets, (enum NON_TERMINAL)(i + NUM_TERMINALS));
+        computeFollowSet(G, firstAndFollowSets, (enum NON_TERMINAL)(i + NUM_TERMINALS), memoized);
     }
+
+    free(memoized);
 
     return firstAndFollowSets;
 }
 
-void createParseTable(FirstAndFollow *F, ParseTable T, Grammar G) {
-    // init empty table
-    for (int i = 0; i < NUM_NON_TERMINALS; i++) {
-        for (int j = 0; j < NUM_TERMINALS; j++) {
-            T[i * NUM_TERMINALS + j] = NULL;
+
+
+// Testing and Verifying
+void printFirstAndFollowSets(Grammar G) {
+
+    // Open the file for writing
+    FILE* file = fopen("firstAndFollowTest.txt", "w");
+    if (!file) {
+        perror("Failed to open file");
+        return;
+    }
+    printf("File Opened Successfully\n");
+
+    FirstAndFollow* sets = ComputeFirstAndFollowSets(G);    
+    printf("Sets Created Successfully");
+
+    for (int i = 0; i < NT_A + 1; i++) {
+        fprintf(file, "Non-terminal: %d\n", i);
+
+        fprintf(file, "FIRST set: ");
+        for (int j = 0; j < sets[i]->firstSetSize; j++) {
+            fprintf(file, "%d ", sets[i]->firstSet[j]);
         }
+        if (sets[i]->firstSetEpsilon) {
+            fprintf(file, "ε");
+        }
+        fprintf(file, "\n");
+
+        // fprintf(file, "FOLLOW set: ");
+        // for (int j = 0; j < sets[i]->followSetSize; j++) {
+        //     fprintf(file, "%d ", sets[i]->followSet[j]);
+        // }
+        // if (sets[i]->followSetDollar) {
+        //     fprintf(file, "$");
+        // }
+        // fprintf(file, "\n\n");
     }
 
-    for (int i = 0; i < G->numRules; i++) {
-        Rule rule = G->rules[i];
-        int row_index = rule->lhs - NUM_TERMINALS; // Normalize non-terminal index
+    fclose(file); // Close the file
 
-        // first set
-        for (int j = 0; j < F[row_index]->firstSetSize; j++) {
-            enum TOKEN_TYPE terminal = F[row_index]->firstSet[j];
-            T[row_index * NUM_TERMINALS + terminal] = rule; 
-        }
-        
-        // epsilon case using follow set
-        if (rule->rhsLength == 1 && rule->rhs[0]->isEpsilon) {
-            for (int j = 0; j < F[row_index]->followSetSize; j++) {
-                enum TOKEN_TYPE terminal = F[row_index]->followSet[j];
-                T[row_index * NUM_TERMINALS + terminal] = rule;
-            }
-        }
+    // Clean up
+    for (int i = 0; i < NT_A + 1; i++) {
+        free(sets[i]->firstSet);
+        free(sets[i]->followSet);
+        free(sets[i]);
     }
+    free(sets);
 }
 
-ParseTree parseInputSourceCode(char *testcaseFileName, ParseTable T) {
-    // FILE *testcasefile = fopen(testcaseFileName, "r");
-    // TwinBuffer buffer = getStream(testcasefile);
-    // TokenInfo token;
-    // LookupTable lt = create_lookup_table();
-
-    // while ((token = getNextToken(buffer, lt)) != NULL) {
-    // }
-}
-
-void printParseTree(ParseTree PT, char *outfile) {
+int main(){
+    printFirstAndFollowSets(&languageGrammar);
 }
